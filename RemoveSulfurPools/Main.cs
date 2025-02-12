35e73ea00e8cafe05b69a1e4e5955e93c8933cec
Sylvia Oleander-Rothove <sylmarssbm@gmail.com>
2025-02-11 21:44:42 -0600

    wasn't certain the IL hook worked. Probably still need to test this. (never commited all this oops)

--- RemoveSulfurPools/Main.cs
+++ RemoveSulfurPools/Main.cs
@@ -9,6 +9,12 @@ using System.Linq;
 using System.Collections;
 using R2API.Utils;
 using R2API.ContentManagement;
+using On.RoR2;
+using System.Collections.Generic;
+using MonoMod.RuntimeDetour;
+using System.Reflection;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
 
 namespace SylmarDev.RemoveSulfurPools
 {
@@ -23,6 +29,10 @@ namespace SylmarDev.RemoveSulfurPools
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginVersion = "1.0.0";
 
+        public List<string> stagesToRemove = new List<string>() { "sulfurpools" };
+
+        private ILHook _hook;
+
         //The Awake() method is run at the very start when the game is initialized.
         public void Awake()
         {
@@ -33,20 +43,66 @@ namespace SylmarDev.RemoveSulfurPools
 
             Log.LogInfo("Assigning hooks. . .");
 
-            On.RoR2.SceneCatalog.SetSceneDefs += SetSceneDefsHook;
+            On.RoR2.Run.CanPickStage += Run_CanPickStage;
+            //On.RoR2.BazaarController.Awake += BazaarController_Awake;
+            
 
             // This line of log will appear in the bepinex console when the Awake method is done.
             Log.LogInfo(nameof(Awake) + " done.");
         }
 
-        private IEnumerator SetSceneDefsHook(On.RoR2.SceneCatalog.orig_SetSceneDefs orig, SceneDef[] newSceneDefs)
+        private void BazaarController_Awake(On.RoR2.BazaarController.orig_Awake orig, RoR2.BazaarController self)
         {
-            // debug
-            Log.LogInfo($"BEFORE scene def names: {String.Join(",", newSceneDefs.ToList().Select(x => x.cachedName))}");
-            newSceneDefs = newSceneDefs.ToList().Where(x => x.cachedName != "sulfurpools").ToArray();
-            Log.LogInfo($"AFTER scene def names: {String.Join(",", newSceneDefs.ToList().Select(x => x.cachedName))}");
-            return orig(newSceneDefs);
+            MethodInfo baazarSetUpSeerStations = typeof(RoR2.BazaarController).GetMethod("SetUpSeerStations", BindingFlags.Instance | BindingFlags.Public);
+
+            // init ILHook
+            _hook = new ILHook(baazarSetUpSeerStations, seerStationsHook);
+
+            orig(self);
+            //throw new NotImplementedException();
         }
+
+        private bool Run_CanPickStage(On.RoR2.Run.orig_CanPickStage orig, RoR2.Run self, RoR2.SceneDef sceneDef)
+        {
+            var isInvalidStage = stagesToRemove.Contains(sceneDef.cachedName);
+            if (isInvalidStage)
+            {
+                Log.LogMessage($"Stopped invalid stage {sceneDef.cachedName}");
+                return false;
+            }
+            return orig(self, sceneDef);
+        }
+
+        private void seerStationsHook(ILContext il)
+        {
+            var cursor = new ILCursor(il);
+
+            cursor.EmitDelegate<Action>(() => Log.LogInfo("SeerStationsHook has been called"));
+            cursor.Goto(50);
+            cursor.Emit(OpCodes.Ldloc_0);
+            cursor.EmitDelegate<Action<List<RoR2.SceneDef>>>(list =>
+            {
+                Log.LogInfo("SeerStationsHook is removing bad stages...");
+                list = list.Where(x => !stagesToRemove.Contains(x.cachedName)).ToList();
+            });
+        }
+
+
+        //private IEnumerator SetSceneDefsHook(On.RoR2.SceneCatalog.orig_SetSceneDefs orig, RoR2.SceneDef[] newSceneDefs)
+        //{
+        //    // debug
+        //    Log.LogInfo($"BEFORE scene def names: {String.Join(",", newSceneDefs.ToList().Select(x => x.cachedName))}");
+        //    newSceneDefs = newSceneDefs.ToList().Where(x => x.cachedName != "sulfurpools").ToArray();
+        //    Log.LogInfo($"AFTER scene def names: {String.Join(",", newSceneDefs.ToList().Select(x => x.cachedName))}");
+
+        //    var returnValue = orig(newSceneDefs ?? new RoR2.SceneDef[] { new RoR2.SceneDef() });
+
+        //    Log.LogInfo($"allBaseSceneNames: {String.Join(",", RoR2.SceneCatalog.allBaseSceneNames.ToList())}");
+        //    Log.LogInfo($"allSceneDefs: {String.Join(",", RoR2.SceneCatalog.allSceneDefs.ToList().Select(x => x.cachedName))}");
+        //    Log.LogInfo($"allStageSceneDefs: {String.Join(",", RoR2.SceneCatalog.allStageSceneDefs.ToList().Select(x => x.cachedName))}");
+
+        //    return returnValue;
+        //}
 
         private void Update()
         {
